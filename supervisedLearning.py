dcdee31eda80e34b0344133ec4504af90324ec62
mstewart2150 <33644856+mstewart2150@users.noreply.github.com>
2023-04-09 18:56:52 -0700

    Update supervisedLearning.py

--- supervisedLearning.py
+++ supervisedLearning.py
@@ -87,3 +87,77 @@ print('Data shape:', boston.data.shape)
 
 X, y = mglearn.datasets.load_extended_boston()
 print('X.shape:', X.shape)
+
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+mglearn.plots.plot_knn_classification(n_neighbors=7)
+
+# * use new data, using methods from Chapter 1 to split out data
+from sklearn.model_selection import train_test_split
+X, y = mglearn.datasets.make_forge()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3)
+
+# * now fit the classifier using the training set
+clf.fit(X_train, y_train)
+
+# * to make a prediction on the test data, we call the predict()
+# * method. For each data point in the test set, this computes
+# * its nearest neighbors in the training set and finds
+#* the common class among these
+
+print('Test set predictions:', clf.predict(X_test))
+
+#* to evaluate how well model generalizes, we can call score()
+#* method
+print('Test set accuracy: {:.2f}'.format(clf.score(X_test, y_test)))
+
+#* we are able to visualize the decision boundary for each k
+
+fig, axes = plt.subplots(1, 3, figsize = (10,3))
+
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+    #* the fit() method returns the object self, so we can
+    #* instantiate and fit in one line
+    mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5,
+                                    ax=ax, alpha=0.4)
+    mglearn.discrete_scatter(X[:,0], X[:,1], y, ax=ax)
+    ax.set_title('{} neighbor(s)'.format(n_neighbors))
+    ax.set_xlabel('Feature 0')
+    ax.set_ylabel('Feature 1')
+axes[0].legend(loc=3)
+
+#* trying this on breast cancer data
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(cancer.data,
+                                                    cancer.target,
+                                                    stratify=cancer.target,
+                                                    random_state=66)
+
+training_accuracy = []
+test_accuracy = []
+# try n_neighbors from 1 to 10
+neighbors_settings = range(1,11)
+
+for n_neighbors in neighbors_settings:
+    # build model
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(clf.score(X_train, y_train))
+    # record test generalization accuracy
+    test_accuracy.append(clf.score(X_test, y_test))
+
+plt.plot(neighbors_settings, training_accuracy, label='training accuracy')
+plt.plot(neighbors_settings, test_accuracy, label='test accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('n_neighbors')
+plt.legend()
+plt.show()
+
